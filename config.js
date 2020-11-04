73d64f441f1d6435a9a6871ea1d352c78db1a2a4
josuecedeno <josuecedeno@yahoo.com>
2020-11-04 16:01:14 -0600

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
   secretKey: '12345-67890-09876-54321',
   mongoUrl: 'mongodb://localhost:27017/nucampsite',
+  facebook: {
+    clientId: '991354504704175',
+    clientSecret: '255d8bb9e6a5d2b7016c3f0b5d35d092',
+  },
 };
