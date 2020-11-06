739b567e9a77d3ab69ad733f923a0e2505aa3bce
josuecedeno <josuecedeno@yahoo.com>
2020-11-06 16:03:36 -0600

    Week 4

--- routes/favoriteRouter.js
+++ routes/favoriteRouter.js
@@ -49,7 +49,6 @@ favoriteRouter
           });
         }
       })
-
       .catch((err) => next(err));
   })
   .put(cors.corsWithOptions, authenticate.verifyUser, (req, res) => {
@@ -74,9 +73,61 @@ favoriteRouter
 favoriteRouter
   .route('/:campsiteId')
   .options(cors.corsWithOptions, (req, res) => res.sendStatus(200))
-  .get(cors.cors, authenticate.verifyUser, (req, res, next) => {})
-  .post(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {})
-  .put(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {})
+  .get(cors.cors, authenticate.verifyUser, (req, res, next) => {
+    res.statusCode = 403;
+    res.end(
+      'GET operation not supported on /favorites/' + req.params.campsiteId
+    );
+  })
+  .post(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
+    Favorite.findOne({ user: req.user._id })
+      .then((favorite) => {
+        if (favorite) {
+          if (favorite.campsites.includes(req.params.campsiteId)) {
+            console.log('Already in Favorites', req.params.campsiteId);
+          } else {
+            console.log('Need to add this one', req.params.campsiteId);
+            favorite.campsites.push(req.params.campsiteId);
+          }
+
+          favorite.save().then((favorite) => {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json(favorite);
+          });
+        } else {
+          console.log('Add new Favorite');
+          const newFavorite = {
+            user: req.user._id,
+            campsites: [req.params.campsiteId],
+          };
+
+          Favorite.create(newFavorite).then((favorite) => {
+            console.log('Favorite created ', favorite);
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json(favorite);
+          });
+
+          // Favorite.create(req.body).then((favorite) => {
+          //   console.log('Favorite created ', favorite);
+          //   favorite.campsites.push(req.params.campsiteId);
+          //   favorite.save().then((favorite) => {
+          //     res.statusCode = 200;
+          //     res.setHeader('Content-Type', 'application/json');
+          //     res.json(favorite);
+          //   });
+          // });
+        }
+      })
+      .catch((err) => next(err));
+  })
+  .put(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
+    res.statusCode = 403;
+    res.end(
+      'PUT operation not supported on /favorites/' + req.params.campsiteId
+    );
+  })
   .delete(
     cors.corsWithOptions,
     authenticate.verifyUser,
